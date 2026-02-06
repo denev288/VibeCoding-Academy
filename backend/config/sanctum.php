84ff211918d05f32dccfe9afac1e36a14cfc1a4f
denev288 <denev288@gmail.com>
2026-02-06 09:31:17 +0200

    session fixes

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -61,6 +61,8 @@ return [
     'middleware' => [
         'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
         'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
+        'add_queued_cookies' => Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        'start_session' => Illuminate\Session\Middleware\StartSession::class,
     ],
 
 ];
